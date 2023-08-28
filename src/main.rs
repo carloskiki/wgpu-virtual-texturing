e2cb14b917b0fe2abb7684c48c5d9d4ffe8d1aa1
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-08-28 05:59:17 -0400

    Refactored Virtual Pipelines methods

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use virt_texture::{
     setup::{VirtualTexturingPipelines, WgpuContext},
     vertex::FOUR_TRIANGLES,
 };
-use wgpu::util::DeviceExt;
+use winit::platform::run_return::EventLoopExtRunReturn;
 
 fn main() {
-    let event_loop = winit::event_loop::EventLoop::new();
+    let mut event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::WindowBuilder::new()
         .with_title("Virtual Texturing Demo")
         .build(&event_loop)
@@ -14,37 +14,7 @@ fn main() {
     let wgpu_ctx = pollster::block_on(WgpuContext::new(window));
     let mut pipelines = VirtualTexturingPipelines::new(&wgpu_ctx, &[]);
 
-    let vertex_buffer = wgpu_ctx
-        .device
-        .create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("vertex buffer"),
-            contents: bytemuck::cast_slice(&FOUR_TRIANGLES),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-
-    let lod_bias_buffer = wgpu_ctx
-        .device
-        .create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("lod bias buffer"),
-            contents: bytemuck::cast_slice(&[0.0f32]),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        });
-    let lod_bias_bind_group = wgpu_ctx
-        .device
-        .create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("lod bias bind group"),
-            layout: &pipelines.lod_bias_bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                    buffer: &lod_bias_buffer,
-                    offset: 0,
-                    size: None,
-                }),
-            }],
-        });
-
-    event_loop.run(move |event, _, control_flow| match event {
+    event_loop.run_return(|event, _, control_flow| match event {
         winit::event::Event::WindowEvent { event, .. } => match event {
             winit::event::WindowEvent::CloseRequested => {
                 *control_flow = winit::event_loop::ControlFlow::Exit
@@ -59,18 +29,11 @@ fn main() {
                     .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                         label: Some("prepass encoder"),
                     });
-            pipelines.prepass(
-                &mut command_encoder,
-                &vertex_buffer,
-                12,
-                &lod_bias_bind_group,
-            );
+            pipelines.set_lod_bias(0.0, &mut command_encoder);
+            pipelines.prepass(&mut command_encoder, &FOUR_TRIANGLES);
+            let output = pipelines.debug_prepass_render(&mut command_encoder);
 
-            let output = pipelines.debug_prepass_render(&wgpu_ctx, &mut command_encoder);
-
-            wgpu_ctx
-                .queue
-                .submit(std::iter::once(command_encoder.finish()));
+            wgpu_ctx.queue.submit(Some(command_encoder.finish()));
             output.present();
         }
         _ => (),
--- src/setup.rs
+++ src/setup.rs
@@ -1,10 +1,12 @@
 use std::num::NonZeroU64;
 
+use wgpu::util::DeviceExt;
+
 pub struct WgpuContext {
     pub surface: wgpu::Surface,
     pub surface_format: wgpu::TextureFormat,
-    pub window: winit::window::Window,
-    pub window_size: winit::dpi::PhysicalSize<u32>,
+    window: winit::window::Window,
+    window_size: winit::dpi::PhysicalSize<u32>,
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
 }
@@ -69,7 +71,9 @@ impl WgpuContext {
     }
 }
 
-pub struct VirtualTexturingPipelines {
+pub struct VirtualTexturingPipelines<'a> {
+    device: &'a wgpu::Device,
+    surface: &'a wgpu::Surface,
     prepass_pipeline: wgpu::RenderPipeline,
     prepass_texture: wgpu::Texture,
     prepass_depth_texture: wgpu::Texture,
@@ -77,12 +81,14 @@ pub struct VirtualTexturingPipelines {
     render_depth_texture: wgpu::Texture,
     #[cfg(debug_assertions)]
     debug_prepass_pipeline: wgpu::RenderPipeline,
-    pub lod_bias_bind_group_layout: wgpu::BindGroupLayout,
+    vertices: Option<(wgpu::Buffer, u32)>,
+    lod_bias_buffer: wgpu::Buffer,
+    lod_bias_bind_group: wgpu::BindGroup,
 }
 
-impl VirtualTexturingPipelines {
+impl<'a> VirtualTexturingPipelines<'a> {
     /// The bind group layouts for the render pipeline.
-    pub fn new(context: &WgpuContext, bind_group_layouts: &[&wgpu::BindGroupLayout]) -> Self {
+    pub fn new(context: &'a WgpuContext, bind_group_layouts: &[&wgpu::BindGroupLayout]) -> Self {
         let shader = context
             .device
             .create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
@@ -140,6 +146,23 @@ impl VirtualTexturingPipelines {
                         count: None,
                     }],
                 });
+        let lod_bias_buffer = context.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("prepass lod bias buffer"),
+            size: std::mem::size_of::<f32>() as u64,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        let lod_bias_bind_group = context
+            .device
+            .create_bind_group(&wgpu::BindGroupDescriptor {
+                label: Some("prepass lod bias bind group"),
+                layout: &lod_bias_bind_group_layout,
+                entries: &[wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: lod_bias_buffer.as_entire_binding(),
+                }],
+            });
+
         let prepass_bind_group_layouts: Vec<&wgpu::BindGroupLayout> =
             [&[&lod_bias_bind_group_layout], &bind_group_layouts[..]].concat();
         let prepass_pipeline_layout =
@@ -246,18 +269,16 @@ impl VirtualTexturingPipelines {
                     .device
                     .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
                         label: Some("debug prepass texture bind group layout"),
-                        entries: &[
-                            wgpu::BindGroupLayoutEntry {
-                                binding: 0,
-                                visibility: wgpu::ShaderStages::FRAGMENT,
-                                ty: wgpu::BindingType::Texture {
-                                    multisampled: false,
-                                    view_dimension: wgpu::TextureViewDimension::D2,
-                                    sample_type: wgpu::TextureSampleType::Uint,
-                                },
-                                count: None,
+                        entries: &[wgpu::BindGroupLayoutEntry {
+                            binding: 0,
+                            visibility: wgpu::ShaderStages::FRAGMENT,
+                            ty: wgpu::BindingType::Texture {
+                                multisampled: false,
+                                view_dimension: wgpu::TextureViewDimension::D2,
+                                sample_type: wgpu::TextureSampleType::Uint,
                             },
-                        ],
+                            count: None,
+                        }],
                     });
 
             let pipeline_layout =
@@ -296,23 +317,46 @@ impl VirtualTexturingPipelines {
         };
 
         Self {
+            device: &context.device,
+            surface: &context.surface,
+            vertices: None,
             prepass_pipeline,
             prepass_texture,
             prepass_depth_texture,
             render_pipeline,
             render_depth_texture,
-            lod_bias_bind_group_layout,
             #[cfg(debug_assertions)]
             debug_prepass_pipeline,
+            lod_bias_bind_group,
+            lod_bias_buffer,
         }
+    }
+
+    /// Set the level of detail bias for the following passes.
+    ///
+    /// The level of detail is used during the prepass to determine which mip level to use for each
+    /// texture page.
+    pub fn set_lod_bias(&mut self, lod_bias: f32, command_encoder: &mut wgpu::CommandEncoder) {
+        let lod_bias_stg = self
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("lod bias stg"),
+                contents: bytemuck::cast_slice(&[lod_bias]),
+                usage: wgpu::BufferUsages::COPY_SRC,
+            });
+        command_encoder.copy_buffer_to_buffer(
+            &lod_bias_stg,
+            0,
+            &self.lod_bias_buffer,
+            0,
+            std::mem::size_of::<f32>() as wgpu::BufferAddress,
+        );
     }
 
     pub fn prepass(
         &mut self,
         command_encoder: &mut wgpu::CommandEncoder,
-        vertex_buffer: &wgpu::Buffer,
-        vertex_count: usize,
-        lod_bias_bind_group: &wgpu::BindGroup,
+        vertices: &[super::vertex::Vertex],
     ) {
         let prepass_view = self
             .prepass_texture
@@ -320,6 +364,14 @@ impl VirtualTexturingPipelines {
         let prepass_depth_view = self
             .prepass_depth_texture
             .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let vertex_buffer = self
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("vertex buffer"),
+                contents: bytemuck::cast_slice(vertices),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
 
         let mut render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("prepass render pass"),
@@ -340,24 +392,23 @@ impl VirtualTexturingPipelines {
                 stencil_ops: None,
             }),
         });
-
         render_pass.set_pipeline(&self.prepass_pipeline);
         render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-        render_pass.set_bind_group(0, lod_bias_bind_group, &[]);
-        render_pass.draw(0..vertex_count as u32, 0..1);
+        render_pass.set_bind_group(0, &self.lod_bias_bind_group, &[]);
+        render_pass.draw(0..vertices.len() as u32, 0..1);
+        drop(render_pass);
+
+        self.vertices = Some((vertex_buffer, vertices.len() as u32));
     }
 
-    pub fn render(
-        &self,
-        command_encoder: &mut wgpu::CommandEncoder,
-        surface: &wgpu::SurfaceTexture,
-        vertex_buffer: &wgpu::Buffer,
-        vertex_count: usize,
-    ) {
-        let ref view = surface
+    pub fn render(&self, command_encoder: &mut wgpu::CommandEncoder) -> wgpu::SurfaceTexture {
+        let output = self.surface.get_current_texture().unwrap();
+        let ref view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
         let ref depth_view = self.render_depth_texture.create_view(&Default::default());
+
+        let (vertices, vertex_len) = self.vertices.as_ref().unwrap();
 
         let mut render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("render pass"),
@@ -379,34 +430,34 @@ impl VirtualTexturingPipelines {
             }),
         });
 
+
         render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-        render_pass.draw(0..vertex_count as u32, 0..1);
+        render_pass.set_vertex_buffer(0, vertices.slice(..));
+        render_pass.draw(0..*vertex_len, 0..1);
+        
+        output
     }
 
     #[cfg(debug_assertions)]
     pub fn debug_prepass_render(
         &self,
-        context: &WgpuContext,
         command_encoder: &mut wgpu::CommandEncoder,
     ) -> wgpu::SurfaceTexture {
-        let output = context.surface.get_current_texture().unwrap();
+        let output = self.surface.get_current_texture().unwrap();
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
 
-        let texture_bind_group = context
-            .device
-            .create_bind_group(&wgpu::BindGroupDescriptor {
-                label: Some("debug prepass texture bind group"),
-                layout: &self.debug_prepass_pipeline.get_bind_group_layout(0),
-                entries: &[wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(
-                        &self.prepass_texture.create_view(&Default::default()),
-                    ),
-                }],
-            });
+        let texture_bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("debug prepass texture bind group"),
+            layout: &self.debug_prepass_pipeline.get_bind_group_layout(0),
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(
+                    &self.prepass_texture.create_view(&Default::default()),
+                ),
+            }],
+        });
 
         let mut render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("debug prepass render pass"),
@@ -424,7 +475,6 @@ impl VirtualTexturingPipelines {
         render_pass.set_pipeline(&self.debug_prepass_pipeline);
         render_pass.set_bind_group(0, &texture_bind_group, &[]);
         render_pass.draw(0..3, 0..1);
-        drop(render_pass);
 
         output
     }
